9c7cbe60760286be6a1438e34982ae48f7e2a917
Dhanushree B <86241933+dhanushreeB@users.noreply.github.com>
2022-05-15 21:21:40 +0530

    Add files via upload

---
+++ BalancedBrackets.java
@@ -0,0 +1,44 @@
+//check for parentheses
+import java.util.*;
+
+public class BalancedBrackets{
+    static boolean areBracketBalanced(String expr){
+        Deque<Character> stack=new ArrayDeque<Character>();
+        for(int i=0;i<expr.length();i++){
+            char x=expr.charAt(i);
+            if(x=='('||x=='{'||x=='['){
+                stack.push(x);
+                continue;
+            }
+            if(stack.isEmpty())
+               return false;
+            char check;
+            switch(x){
+                case ')':
+                  check=stack.pop();
+                  if(check=='{'||check=='[')
+                    return false;
+                  break;
+                case '}':
+                   check=stack.pop();
+                   if(check=='('||check=='[')
+                     return false;
+                   break;
+                case ']':
+                   check=stack.pop();
+                   if(check=='('||check=='{')
+                      return false;
+                    break;
+
+            }
+        }
+        return (stack.isEmpty());
+    }
+public static void main(String[] args) {
+    String expr="({})(({";
+    if(areBracketBalanced(expr))
+       System.out.println("balanced");
+    else  
+       System.out.println("Not Balanced");
+}
+}
