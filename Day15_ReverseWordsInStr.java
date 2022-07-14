1a388bae94927402220a50e71f7e4bab24cd449b
Dhanushree B <86241933+dhanushreeB@users.noreply.github.com>
2022-07-14 15:15:00 +0530

    Add files via upload

---
+++ Day15_ReverseWordsInStr.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+class Day15_ReverseWordInStr {
+    public static void main(String[] args) {
+        String s = "learning strings now";
+        System.out.println("After reversing words: ");
+        System.out.println(s);
+        s += " ";
+        Stack<String> st = new Stack<String>();
+        int i;
+        String str = "";
+        for (i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                st.push(str);
+                str = "";
+            } else {
+                str += s.charAt(i);
+            }
+        }
+        String ans = "";
+        while (st.size() != 1) {
+            ans += st.peek() + " ";
+            st.pop();
+        }
+        ans += st.peek(); // The last word should'nt have a space after it
+        System.out.println("After reversing words: ");
+        System.out.print(ans);
+    }
+}
